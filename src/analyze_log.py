f031328a4703fdc575b7364f205d94307ad08940
Jesse Benevides <jessebenevides@gmail.com>
2022-08-18 12:07:30 -0300

    feat: req 1 - add get_data function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,21 @@
+import csv
+
+
+def get_data(path_to_file):
+    if '.csv' not in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file, 'r') as file:
+            reader = csv.reader(file, delimiter=",")
+            data = list(reader)
+            return data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+
 def analyze_log(path_to_file):
-    pass
+    all_data = get_data(path_to_file)
+    print(all_data)
+
+analyze_log("data/orders_1.csv")
